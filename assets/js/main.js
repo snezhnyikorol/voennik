c8015982cd19ecadb7e06ad937e148e78674c21a
Aliaksei Raketski <raketskij@gmail.com>
2019-09-10 00:56:35 +0300

    fix: footer feat: finish questions feat: sticky header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
+$(window).scroll(function(){
+    var sticky = $('.sticky'),
+        scroll = $(window).scrollTop();
+  
+    if (scroll >= 50) {
+      sticky.addClass('fixed');
+    }
+    else {
+      sticky.removeClass('fixed');
+    } 
+  
+    checkMap()
+  });
+  
+
 
 let fetchEr = () => {
     fetch('./assets/json/data.json')
@@ -76,4 +91,125 @@ let render = (data) => {
     });
 }
 
+var spinner = $('.ymap-container').children('.loader');
+
+var check_if_load = false;
+
+var myMapTemp, myPlacemarkTemp;
+ 
+
+function init () {
+  var myMapTemp = new ymaps.Map("map-yandex", {
+    center: [53.906071, 27.558956], 
+    zoom: 18, 
+    controls: ['zoomControl', 'fullscreenControl'],
+  });
+  myMapTemp.behaviors.disable('scrollZoom');
+  var myPlacemarkTemp = new ymaps.Placemark([53.906071, 27.558956], {
+    balloonContent: 'ВОЕННИК.БАЙ<br>г. Минск, ул. Зыбицкая, 4'
+    }, {
+        preset: 'islands#blackHomeIcon'
+    })
+
+  myMapTemp.geoObjects.add(myPlacemarkTemp)
+ 
+  
+  var layer = myMapTemp.layers.get(0).get(0);
+ 
+ 
+  waitForTilesLoad(layer).then(function() {
+ 
+    spinner.removeClass('is-active');
+  });
+}
+ 
+
+function waitForTilesLoad(layer) {
+  return new ymaps.vow.Promise(function (resolve, reject) {
+    var tc = getTileContainer(layer), readyAll = true;
+    tc.tiles.each(function (tile, number) {
+      if (!tile.isReady()) {
+        readyAll = false;
+      }
+    });
+    if (readyAll) {
+      resolve();
+    } else {
+      tc.events.once("ready", function() {
+        resolve();
+      });
+    }
+  });
+}
+ 
+function getTileContainer(layer) {
+  for (var k in layer) {
+    if (layer.hasOwnProperty(k)) {
+      if (
+        layer[k] instanceof ymaps.layer.tileContainer.CanvasContainer
+        || layer[k] instanceof ymaps.layer.tileContainer.DomContainer
+      ) {
+        return layer[k];
+      }
+    }
+  }
+  return null;
+}
+ 
+
+function loadScript(url, callback){
+  var script = document.createElement("script");
+ 
+  if (script.readyState){  // IE
+    script.onreadystatechange = function(){
+      if (script.readyState == "loaded" ||
+              script.readyState == "complete"){
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {  // Другие браузеры
+    script.onload = function(){
+      callback();
+    };
+  }
+ 
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+ 
+
+function checkMap() {
+  if (document.getElementById('map').getBoundingClientRect().y < document.documentElement.clientHeight) {
+    
+    if (!check_if_load) { 
+ 
+	  	
+        check_if_load = true; 
+ 
+		
+        spinner.addClass('is-active');
+ 
+	
+        loadScript("https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;loadByRequire=1", function(){
+  
+           ymaps.load(init);
+        });                
+      }
+  }
+}
+
+$('.questions_card').on('click', (e) => {
+    $(e.currentTarget).children('.questions_card-text').toggle('fast')
+    console.log($(e.currentTarget).find('.questions_card-img').css('transform'))
+    if ($(e.currentTarget).find('.questions_card-img').css('transform')!=='none') {
+        $(e.currentTarget).find('.questions_card-img').css('transform', 'none')
+    } else {
+        $(e.currentTarget).find('.questions_card-img').css({
+            'transform': 'rotate(180deg)',
+            'transition': 'all 400ms'
+        })
+    }
+
+})
 
